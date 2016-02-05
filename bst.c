4c5cba2543f77b5ed2fa2595ea86ff01fda15576
Hongguang Gao <hongguang.gao@gmail.com>
2016-02-04 21:47:01 -0800

    Make the program works

--- bst.c
+++ bst.c
@@ -5,6 +5,16 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+typedef struct _queue {
+   int size;
+   int first;
+   int last;
+   int count;
+   void *buffer[0];
+} Queue;
+
+
+
 typedef struct _bstree BSTree;
 struct _bstree {
    int value;
@@ -15,6 +25,98 @@ struct _bstree {
 #define NUM 15
 int arrs_sorted[NUM] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 char bitmap[10][130];
+
+Queue *create_queue(int size)
+{
+   Queue *que;
+   
+   que = malloc(sizeof(Queue) + sizeof(void*) * size);
+   assert(que != NULL);
+   que->size = size;
+   que->first = 0;
+   que->last = 0;
+   que->count = 0;
+
+   return que;
+}
+
+void destroy_queue(Queue *que)
+{
+   free(que);
+}
+
+void enqueue(Queue *que, void *value)
+{
+   assert(que->count < que->size);
+
+   que->buffer[que->last] = value;
+   que->last = (que->last + 1) % que->size;
+   que->count++;
+}
+
+void *dequeue(Queue *que)
+{
+   void *value = NULL;
+
+   if (que->count > 0) {
+      value = que->buffer[que->first];
+      que->first = (que->first + 1) % que->size;
+      que->count--;
+   }
+
+   return value;
+}
+
+int get_queue_count(Queue *que)
+{
+   return que->count;
+}
+
+BSTree *create_bst_from_sorted_array(int arr[], int start, int end)
+{
+   int mid;
+   BSTree *root = NULL;
+
+   if (start > end) {
+      return NULL;
+   }
+
+   mid = (start + end) / 2;
+   root = malloc(sizeof(BSTree));
+   assert(root != NULL);
+   root->value = arr[mid];
+   root->left = create_bst_from_sorted_array(arr, start, mid - 1);
+   root->right = create_bst_from_sorted_array(arr, mid + 1, end);
+
+   return root;
+}
+
+void destroy_bst(BSTree *root)
+{
+   if (root == NULL) {
+      return;
+   }
+
+   if (root->left != NULL) {
+      destroy_bst(root->left);
+   }
+   if (root->right != NULL) {
+      destroy_bst(root->right);
+   }
+   free(root);
+}
+
+int bst_depth(BSTree *root)
+{
+   int lDepth, rDepth;
+
+   if (root == NULL) {
+      return 0;
+   }
+   lDepth = bst_depth(root->left);
+   rDepth = bst_depth(root->right);
+   return MAX(lDepth, rDepth) + 1;
+}
 
 void visit_print_bst(BSTree *root, int row, int col, int w)
 {
@@ -86,5 +188,6 @@ int main(void)
     printf("====== The BST ======\n");
     print_bst(root);
 
+    destroy_bst(root);
     return 0;
 }
